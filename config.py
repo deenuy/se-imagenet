0a81d32cd664ee3b9725dcb081a55e7c8916ea79
gengiti-deenu <deenuy@my.yorku.ca>
2021-05-19 07:48:25 -0400

    migrated db to mysql and completed python wrapper to execute sql queries

---
+++ config.py
@@ -0,0 +1,4 @@
+GOOGLE_API_KEY = 'AIzaSyCWUYTUdn0uDwQC0rGs-AmMHOxnAYyDzRE'
+GOOGLE_SE_ID = 'a0c1cbc36a0e523aa' # Programmable Search Engine ID
+DB_USER = 'root'
+DB_PWD = 'deenanath'
---
+++ mysql_wrapper.py
@@ -0,0 +1,62 @@
+import mysql.connector
+from mysql.connector import errorcode
+import config
+import csv
+import pandas as pd
+
+db_config = {
+  'user': config.DB_USER,
+  'password': config.DB_PWD,
+  'host': 'localhost',
+  'database': 'SEWordSim-r1',
+  'raise_on_warnings': True,
+  'auth_plugin': 'mysql_native_password'
+}
+
+try:
+  cnx = mysql.connector.connect(**db_config)
+except mysql.connector.Error as err:
+  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+    print("Something is wrong with your user name or password")
+  elif err.errno == errorcode.ER_BAD_DB_ERROR:
+    print("Database does not exist")
+  else:
+    print(err)
+
+cnx = mysql.connector.connect(**db_config)
+cursor = cnx.cursor()
+
+# function to execute sql query and print results to csv
+def expQueryResults(query, name):
+  cursor.execute(query)
+  results = cursor.fetchall()
+  df = pd.DataFrame(results)
+  df.to_csv(name+'.csv', index=False, header=False)
+
+def printQryResults(query):
+  cursor.execute(query)
+  results = cursor.fetchall()
+  if len(results)>1:
+    for row in results:
+      print(row)
+  else:
+    print(results)
+
+query2 = "SELECT count(*) FROM `SEWordSim-r1`.Word_Similarity"
+
+# Find synset with term1 having length > 5
+query3 = ("SELECT term1, count(term2) "
+            "FROM `SEWordSim-r1`.Word_Similarity "
+            "GROUP BY term1 "
+            # "HAVING length(term1) >= 5 "
+            "ORDER BY term1 DESC")
+
+# Find synset by term1 keyword
+query4 = "SELECT term1, term2 FROM `SEWordSim-r1`.Word_Similarity WHERE term1 like 'Eclipse'"
+
+printQryResults(query2)
+# expQueryResults(query3, 'synset_count_full')
+expQueryResults(query4, 'synset_eclipse') 
+
+cursor.close()
+cnx.close()
